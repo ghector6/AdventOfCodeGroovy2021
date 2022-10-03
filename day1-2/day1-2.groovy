d246f6b73a14dc573175c3207842269439f3ef72
Hector Garcia Rosales <ghectormus@gmail.com>
2022-10-02 22:39:18 -0500

    Adding range and working closure

--- day1-2/day1-2.groovy
+++ day1-2/day1-2.groovy
@@ -1,7 +1,8 @@
 List numList = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]  
+List flagList = []
 def range = (0..2)
 int sum 
 numList.each{ num ->  
-	println numList 
+	println numList[range]
+	 
 }
-
