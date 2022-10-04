de17dbde4c9f770655500cd2609a2e41c4345e5e
Hector Garcia Rosales <ghectormus@gmail.com>
2022-10-04 12:26:14 -0500

    Adding collate method

--- day1-2/day1-2.groovy
+++ day1-2/day1-2.groovy
@@ -3,9 +3,18 @@ List flagList = []
 def range = (0..2)
 int sum 
 
-while(numList.size() !=  2){
+/*while(numList.size() !=  2){
 	numList = numList[1..-1]
 	println numList
-	}
+	}*/
+flagList = numList.collate(3,1)
+
+flagList.each{ block ->
+	for(num in block){
+		sum = num + sum
+		println sum
+	} 
+
+}
 	
 
