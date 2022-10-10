a5bada0fe29b3b1994487d88c013d62a3424be23
Hector Garcia Rosales <ghectormus@gmail.com>
2022-10-10 08:45:04 -0500

    Adding list and an index counter thinking in microservices for this one

--- day3/day3-1.groovy
+++ day3/day3-1.groovy
@@ -1,12 +1,23 @@
 def test = new File("/Users/ghector6/MakingDevsProjects/AdventOfCode/day3/inputday3.txt").text
 
-def zeroFlag = 0
+def zeroFlag = []
 def oneFlag = 0
+def ceroflag = 0
 
 test.eachLine{line ->
 	line.eachWithIndex{num, index ->
-		if(index == 0){
-			println num
+		if(index == 1){
+			zeroFlag << num
 		}
 	}
 }
+zeroFlag.findAll{element -> 
+	if(element == "1"){
+		oneFlag ++
+	}else if(element == "0"){
+		ceroflag++
+	}
+
+} 
+
+println ceroflag
